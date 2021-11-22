6f2e59368a474c22fe0d92de70e90d8231342d13
ginoruperez <ginoruperez@gmail.com>
2021-11-22 23:09:47 +0000

    Update cabackup.sh

--- cabackup.sh
+++ cabackup.sh
@@ -95,7 +95,7 @@ while read line; do
 
 	# tar -czvf /var/backup.tar.gz $USER_DIRECTORY  this is final 
 	echo "Creating a backup.tar.gz file in /var from /var/backup content"
-	sudo tar -czvf ${VAR_DIR}/backup.tar.gz  ${DIRECTORY_TO_BACKUP}
+	sudo tar -C ${DIRECTORY_TO_BACKUP} -cvf backup.tar.gz . 
 
 	#display the list of /var/backup.tar.gz
 	tar --list --file=/var/backup.tar.gz
