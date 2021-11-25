503f6fb079fd8d7a87336930f5c9416ff5c95d63
ginoruperez <ginoruperez@gmail.com>
2021-11-25 23:01:49 +0000

    Update cabackup.sh

--- cabackup.sh
+++ cabackup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-#logger variable 
+# logger variable 
 LOGGER=/usr/bin/logger
 
-#this variable is switch to true once .tar.gz is extracted in /tmp/backup
+# this variable is switch to true once .tar.gz is extracted in /tmp/backup
 ISTARFILE_EXTRACTED=false
 
-#Initial file to be checked or created if .backup file is not yet existed
+# Initial file to be checked or created if .backup file is not yet existed
 BACKUPFILE=.backup
 
-#Folder to zip and copy the files to be backup
+# Folder to zip and copy the files to be backup
 VAR_DIR=/var  
 
-#Folder to extract existing backup.tar.gz
+# Folder to extract existing backup.tar.gz
 TMP_BACKUP=/tmp/backup
 
-#Source folder to be zip into backup.tar.gz
+# Source folder to be zip into backup.tar.gz
 DIRECTORY_TO_BACKUP=${VAR_DIR}/backup
 
-#Display usage instruction 
+# Display usage instruction 
 display_usage() { 
 	echo -e "\nThis script must be run with a sudo access privilege." 
 	echo "Make sure argument passed is a file containing existing users list of this system e.g userlist.txt"
 	echo -e "\nUsage: $0 [argument] \n" 
 	} 
 
-#Create logs in /var/log/syslog if runs in ubuntu or /var/log/messages if it runs in other distro e.g. centos
+# Create logs in /var/log/syslog if runs in ubuntu or /var/log/messages if it runs in other distro e.g. centos
 info()
 {
     ${LOGGER} -s -t CABACKUP-SCRIPT -p user.notice "INFO: $@"
@@ -36,7 +36,7 @@ error()
     ${LOGGER} -s -t CABACKUP-SCRIPT -p user.err "ERROR: $@"
 }
 
-#Check filename contains / symbol listed in .backup file
+# Check filename contains / symbol listed in .backup file
 chk_filename() {
 
 	STRFILE=$1
@@ -56,19 +56,19 @@ then
 	exit 0
 fi 
 
-#Check if parameter is passed or valid, otherwise terminate the program
+# Check if parameter is passed or valid, otherwise terminate the program
 if [ ! -f $1 ]; then
 	display_usage
 	exit 1
 fi
 
-#if no parameter passed, call display_usage and terminate the program
+# if no parameter passed, call display_usage and terminate the program
 if [ $# -eq 0 ]; then
     display_usage
     exit 1
 fi
 
-#Check if user has sudo access privelege
+# Check if user has sudo access privelege
 printf "skippass\n" | sudo -S /bin/chmod --help >/dev/null 2>&1
 info "Checking user's privileges..."
 if [ $? -eq 0 ];then
@@ -79,7 +79,7 @@ else
    exit 0
 fi
 
-#Iterate base on the number lines entered in userlist text file
+# Iterate base on the number lines entered in userlist text file
 while read line; do
 
 	USER=$line
@@ -104,16 +104,16 @@ while read line; do
 		fi
 	fi
 
- 	#check if /var/backup.tar.gz exist
+ 	# check if /var/backup.tar.gz exist
  	if [ -f ${VAR_DIR}/backup.tar.gz ]; then
 
 
 		if [ "$ISTARFILE_EXTRACTED" = false ]; then
 
 			info "File /var/backup.tar.gz existed"
-			#check if /tmp/backup dir exist before extracting remove and create
+			# check if /tmp/backup dir exist before extracting remove and create
 			if [ -d /tmp/backup ]; then 
-				#remove the existing files from /tmp/backup if any
+				# remove the existing files from /tmp/backup if any
 				sudo rm -rf /tmp/backup
 			fi
 			sudo mkdir /tmp/backup
@@ -121,26 +121,26 @@ while read line; do
 			info "Extracting /var/backup.tar.gz to /tmp/backup once only"
 			sudo tar xf ${VAR_DIR}/backup.tar.gz -C $TMP_BACKUP
 
-			#set the switch to true
+			# set the switch to true
 			ISTARFILE_EXTRACTED=true
 
 		fi
 
-		#Put /home/username/.backup to USER_BACKUP_FILE
+		# Put /home/username/.backup to USER_BACKUP_FILE
 		USER_BACKUP_FILE=${USER_DIRECTORY}/$BACKUPFILE
 
 		while read USERFILELINE; do
 
-			#This will contain a value /home/username/filename
+			# This will contain a value /home/username/filename
 			FILE1=${USER_DIRECTORY}/$USERFILELINE
 
-			#This will contain a value /tmp/backup/username/filename
+			# This will contain a value /tmp/backup/username/filename
 			FILE2=${TMP_BACKUP}/$USER/$USERFILELINE
 
-			#Check filename if contains / special symbol or the file is placed in a subdirectory relative to user directory
+			# Check filename if contains / special symbol or the file is placed in a subdirectory relative to user directory
 			chk_filename ${USERFILELINE}
 
-			#if the same file exist in /tmp/backup/user then start the comparison and renaming of old files
+			# if the same file exist in /tmp/backup/user then start the comparison and renaming of old files
 			if [ -f $FILE2 ]; then
 
 				
@@ -152,12 +152,12 @@ while read line; do
 					
 					counter=1
 					until [ ! -f ${FILE2}.$counter ]; do
-						#Increment the counter for file renaming
+						# Increment the counter for file renaming
 						let counter+=1
 						
 					done
 
-					#replace the previous with e.g. filename.1 , filename.2 ... 
+					# replace the previous with e.g. filename.1 , filename.2 ... 
 					info "Renaming ${FILE2} to " ${FILE2}.$counter 
 					sudo mv ${FILE2} ${FILE2}.$counter			
 
@@ -166,11 +166,11 @@ while read line; do
 
 			fi
 
-			#Check if entry in .backup file is existed if not,  log the error
+			# Check if entry in .backup file is existed if not,  log the error
 			if [ ! -f $FILE1 ]; then
 				error "Entry filename $USERFILELINE in .backup file is not existed!"
 			else
-				#Then copy the original  file from /home/user to /tmp/backup/user/
+				# Then copy the original  file from /home/user to /tmp/backup/user/
 				info "Copying  $FILE1 to ${TMP_BACKUP}/$USER"
 				sudo cp $FILE1 ${TMP_BACKUP}/$USER 2>/dev/null
 			
@@ -180,32 +180,32 @@ while read line; do
 		done < $USER_BACKUP_FILE
 
 
-		#Check if /var/backup exist, if not create first
+		# Check if /var/backup exist, if not create first
 		if [ ! -d ${VAR_DIR}/backup ]; then 
 			info "Folder /var/backup  is created"
 			sudo mkdir ${VAR_DIR}/backup
 		fi
 
-		#remove and create /var/backup/user 
+		# remove and create /var/backup/user 
 		VAR_BAK_USER=${VAR_DIR}/backup/$USER
 		if [ -d ${VAR_BAK_USER} ]; then 
 			sudo rm -rf $VAR_BAK_USER  
 		fi
 		sudo mkdir $VAR_BAK_USER
 		
-		#Copying the content of /tmp/backup/user/*.* /var/backup/user"
+		# Copying the content of /tmp/backup/user/*.* /var/backup/user"
 		info "Copying the content of " ${TMP_BACKUP}/${USER}/ " to " $VAR_BAK_USER 
 		sudo cp ${TMP_BACKUP}/${USER}/*.* $VAR_BAK_USER 2>/dev/null
 
  	else
 
-		#Check if /var/backup exist, if not create first
+		# Check if /var/backup exist, if not create first
 		if [ ! -d ${VAR_DIR}/backup ]; then 
 				info "Folder /var/backup folder is created"
 				sudo mkdir ${VAR_DIR}/backup
 		fi
 
-		#Read each user e.g /home/gino/.backup file and copy to /var/backup/$user/
+		# Read each user e.g /home/gino/.backup file and copy to /var/backup/$user/
 		VAR_BAK_USER=${VAR_DIR}/backup/$USER
 		USER_BACKUP_FILE=${USER_DIRECTORY}/$BACKUPFILE
 
@@ -214,13 +214,13 @@ while read line; do
 				sudo mkdir $VAR_BAK_USER
 		fi
 
-		#Read the user .backup file and copy the file listed to /var/backup/$user 
+		# Read the user .backup file and copy the file listed to /var/backup/$user 
 		while read USERFILELINE1; do
 
-			#Check filename if contains / special symbol or the file is placed in a subdirectory relative to user directory
+			# Check filename if contains / special symbol or the file is placed in a subdirectory relative to user directory
 			chk_filename ${USERFILELINE1}
 
-			#copying /home/gino/file to /var/backup/gino
+			# copying /home/gino/file to /var/backup/gino
 			info "Copying file ${USER_DIRECTORY}/$USERFILELINE1 to $VAR_BAK_USER"
 			sudo cp ${USER_DIRECTORY}/$USERFILELINE1 $VAR_BAK_USER
 
@@ -237,7 +237,7 @@ echo ""
 info "Creating a backup.tar.gz file in /var from /var/backup content"
 sudo tar -C $DIRECTORY_TO_BACKUP -cvf ${VAR_DIR}/backup.tar.gz . 
 
-#display the list of /var/backup.tar.gz
+# display the list of /var/backup.tar.gz
 echo ""
 info "Listing the content of /var/backup.tar.gz"
 tar --list --file=/var/backup.tar.gz
